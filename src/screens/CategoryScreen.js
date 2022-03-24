f5fdebb1a581588cdc57a5047c93d2a85f02b666
MosQyy <akbarma2002@gmail.com>
2022-03-24 08:29:41 +0700

    repair status bar categories screen

--- src/screens/CategoryScreen.js
+++ src/screens/CategoryScreen.js
@@ -1,7 +1,15 @@
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
 import React from 'react';
 import {moderateScale} from 'react-native-size-matters';
 import {useNavigation} from '@react-navigation/native';
+import {useIsFocused} from '@react-navigation/native';
 
 import ListGenre from '../components/CategoryScreen/ListGenre';
 import Color from '../config/utils/color';
@@ -10,8 +18,17 @@ import {BackIcon} from '../assets';
 const CategoryScreen = () => {
   const navigation = useNavigation();
 
+  function ScreenStatusBar() {
+    const focus = useIsFocused();
+
+    return focus ? (
+      <StatusBar backgroundColor={Color.BACKGROUND_COLOR} />
+    ) : null;
+  }
+
   return (
     <View style={styles.container}>
+      <ScreenStatusBar />
       <View style={styles.header}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <Image source={BackIcon} style={styles.backIcon} />
